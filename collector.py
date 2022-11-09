d5acea58ccd0891333bf506d32854ba7bbda382d
Craig Mulligan <git@craigmulligan.com>
2022-11-08 21:34:28 -0600

    collector complete

--- collector.py
+++ collector.py
@@ -1,4 +1,6 @@
-from lib.collector import collect_urls
+from lib.collector import collect_recipes, Persister
 
 if __name__ == "__main__":
-    collect_urls("data/url.txt")
+    # collect_urls("data/url.txt")
+    persistor = Persister("data/recipe")
+    collect_recipes(persistor, "data/url.txt")
