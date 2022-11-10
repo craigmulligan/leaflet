bbfb9e3f8146c55cb15381d63367da3d0cf2ee61
Craig Mulligan <git@craigmulligan.com>
2022-11-09 21:36:36 -0600

    add recipe scaling

--- app/recipe_manager/__init__.py
+++ app/recipe_manager/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List
 from flask import g
 from app.models import User, Recipe
@@ -5,17 +6,64 @@ from app import database
 from dataclasses import dataclass
 from datetime import datetime
 
+from pint import UnitRegistry, UndefinedUnitError, DimensionalityError
+
+pint = UnitRegistry()
+
 
 @dataclass
 class Digest:
     created_at: datetime
     recipes: List[Recipe]
-    user_id: int
+    user: User
 
-    def shopping_list(self):
+    def shopping_list(self) -> List[str]:
         """
         For each recipe we and the similar recipes.
         """
+        ingredients = {}
+
+        for recipe in self.recipes:
+            for ingredient in recipe.ingredients:
+                if ingredients.get(ingredient.name):
+                    ingredients[ingredient.name].append(ingredient)
+                else:
+                    ingredients[ingredient.name] = [ingredient]
+
+        failures = []
+        shopping_list = {}
+
+        for key, value in ingredients.items():
+            total = 0
+
+            for v in value:
+                try:
+                    total += pint.Quantity(v.quantity, v.unit)
+                except UndefinedUnitError:
+                    logging.warning(f"couldn't get normalize unit:{v.unit}")
+                    failures.append(v)
+                except ValueError:
+                    logging.warning(f"couldn't get normalize quantity:{v.quantity}")
+                    failures.append(v)
+                except DimensionalityError:
+                    breakpoint()
+
+            if total > 0:
+                shopping_list[key] = total
+
+        output = []
+        for key, value in shopping_list.items():
+            magnitude = round(value.magnitude * self.user.serving, 2)
+            units = "" if value.dimensionless else pint.get_symbol(str(value.units))
+            output.append(f"{key}: {magnitude} {units}".strip())
+
+        for f in failures:
+            output.append(
+                f"{f.name}: {f.quantity * self.user.serving} {f.unit}".strip()
+            )
+
+        output.sort()
+        return output
 
     def to_html(self):
         pass
@@ -51,7 +99,7 @@ class RecipeManager:
             recipes.append(db.recipe_get(recipe_id))
 
         now = datetime.utcnow()
-        return Digest(now, recipes, user.id)
+        return Digest(now, recipes, user)
 
     def save_digest(self, digest):
         for recipe in digest.recipes:
