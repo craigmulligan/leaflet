3e46da9c104eb9b1c29cf7e471f4a6698e4975b4
Craig Mulligan <git@craigmulligan.com>
2022-11-09 21:36:36 -0600

    add recipe scaling

--- tests/test_recipe_manager.py
+++ tests/test_recipe_manager.py
@@ -33,3 +33,30 @@ def test_save_digest(
     recipe_manager.save_digest(digest)
 
     assert len(db.digest_get_all_by_user(user.id)) == 1
+
+
+def test_get_shopping_list(
+    dummy_user,
+    recipe_manager,
+    db,
+    mock_recipe_random,
+    dummy_recipe_id,
+):
+    user = dummy_user()
+    digest = recipe_manager.get_digest(user)
+    shopping_list = digest.shopping_list()
+
+    assert len(shopping_list) == 24
+    last_item = shopping_list[-1]
+    assert last_item == "yellow pepper: 2"
+
+    # now lets scale the servings and check the shopping_list adjusts accordingly.
+    serving_size = 2
+    db.user_update(user.id, user.recipes_per_week, serving_size)
+    user = db.user_get_by_id(user.id)
+    digest = recipe_manager.get_digest(user)
+    shopping_list = digest.shopping_list()
+
+    assert len(shopping_list) == 24
+    last_item = shopping_list[-1]
+    assert last_item == "yellow pepper: 4"
