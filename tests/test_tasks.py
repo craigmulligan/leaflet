794c2309a73199df9dc54ce47c8b9bb7c6ba7d09
Craig Mulligan <git@craigmulligan.com>
2022-12-22 11:39:13 +1300

    Add check to ensure we dont send two scheduled emails per day

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,22 +1,26 @@
-from freezegun import freeze_time
 from app.tasks import daily
 from datetime import datetime, timedelta
 from .conftest import Contains
 
-@freeze_time("2022-10-01")
 def test_daily(dummy_user, mail_manager_mock, db):
     send_at = datetime.utcnow() - timedelta(days=3)
+
+    users = {}
 
     for i in range(5):
         user = dummy_user(email=f"{i}@x.com")
         dt = send_at - timedelta(days=5-i)
+        users[dt.isoweekday()] = user
         db.user_update_send_at(user.id, dt)
         
     daily.delay()
 
+    weekday = datetime.utcnow().isoweekday()
+    user = users[weekday]
+
     # Should only send to user with 1.
     mail_manager_mock.send.assert_called_once_with(
-        "1@x.com",
+        user.email,
         f"Leaflet #1",
         Contains("Shopping List"),
     )
@@ -29,14 +33,3 @@ def test_daily(dummy_user, mail_manager_mock, db):
     daily.delay()
     mail_manager_mock.send.assert_not_called()
     mail_manager_mock.send.reset_mock()
-
-    # Now test the following day.
-    # Should send to user 5
-    with freeze_time("2022-10-02"):
-        daily.delay()
-        mail_manager_mock.send.assert_called_once_with(
-            "2@x.com",
-            f"Leaflet #1",
-            Contains("Shopping List"),
-        )
-
