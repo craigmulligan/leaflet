5481b121b1382381cfbf812ad229eabd674cfb17
Craig Mulligan <git@craigmulligan.com>
2024-05-04 15:29:06 -0400

    set cookie

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from starlette.middleware.sessions import SessionMiddleware
+
 from app.models import BaseModel
 from app.db import engine
 from app.routes import magic
+from app.routes import dashboard
+from app.config import SECRET_KEY
 
 BaseModel.metadata.create_all(bind=engine)
 app = FastAPI()
+app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.include_router(magic.router)
+app.include_router(dashboard.router)
--- app/routes/magic.py
+++ app/routes/magic.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, Request, Depends, UploadFile, File, HTTPException
+from fastapi import APIRouter, Request, Depends, UploadFile, File, HTTPException, Query 
+from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
+from itsdangerous import BadSignature, SignatureExpired
 
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app import models
 from app.depends import get_db
-from app.config import HOST_URL, IS_DEV
+from app.config import IS_DEV
 
 
 router = APIRouter()
@@ -29,15 +31,39 @@ def magic(request: Request, email: UploadFile = File(...), db: Session = Depends
 
     # User is now created or present
     token = user.get_signin_token()
-    magic_path = f"/magic?token={token}"
-    magic_markup = None
+    magic_url = f"/magic?token={token}"
 
-    if IS_DEV:
-        magic_markup = f"<a href={magic_path}>Click here to signin.</a>"
-    else:
-        pass
+    if not IS_DEV:
         ## TODO send email
+        pass
         # (user.email, "Signin link", f"{HOST_URL}/{magic_path}")
 
-    return templates.TemplateResponse(request, "dashboard.html", {"email": user.email, "magic_markup": magic_markup })
+    return templates.TemplateResponse(request, "dashboard.html", {"email": user.email, "magic_url": magic_url if IS_DEV else None })
 
+
+@router.get("/magic")
+async def magic_get(request: Request, token: str = Query(...)):
+    """
+    Handler for the GET /magic endpoint with a 'token' query parameter.
+    """
+    if not token:
+        raise HTTPException(status_code=400, detail="A token is required to signin")
+
+    try:
+        user_id = models.User.verify_signin_token(token)
+    except SignatureExpired:
+        raise HTTPException(status_code=403, detail="Your link has expired")
+    except BadSignature:
+        raise HTTPException(status_code=403)
+
+    response = RedirectResponse("/dashboard")
+    response.set_cookie(key="user_id", value=user_id, httponly=True)  # Store user_id in HTTP cookie
+    return response 
+
+
+@router.get("/signin")
+async def signin_get(request: Request):
+    """
+    signin form
+    """
+    return "hi" 
