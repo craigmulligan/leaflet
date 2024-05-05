98d8d513ae5bed788af297647647b163aca5b409
Craig Mulligan <git@craigmulligan.com>
2024-05-05 11:33:56 -0400

    add candidates test

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -19,11 +19,11 @@ def test_generate(db: Session, create_user):
 def test_get_candidates(db: Session, create_user):
     """
     Create 15 users 
-    users 1-5: have leaflet never received a leaflet.
+    users 1-5: have never received a leaflet.
 
     users 5-10: have a leaflet within a week old.
 
-    users 10-15 have a leaflet more than a week old.
+    users 10-15 have no leaflets within a week old.
     """
 
     expected_candidates: List[models.User] = []
@@ -44,10 +44,12 @@ def test_get_candidates(db: Session, create_user):
         if 10 < n <= 15:
             expected_candidates.append(user)
 
-            leaflet = models.Leaflet()
-            db.add(leaflet)
-            leaflet.user = user
-            leaflet.created_at = datetime.now() - timedelta(days=random.randint(8, 14))
+            for i in range(1, 5):
+                # lets create a bunch of old leaflets for these users.
+                leaflet = models.Leaflet()
+                db.add(leaflet)
+                leaflet.user = user
+                leaflet.created_at = datetime.now() - timedelta(days=random.randint(8*i, 14*i))
 
     assert len(expected_candidates) == 10
     manager = LeafletManager(db)
