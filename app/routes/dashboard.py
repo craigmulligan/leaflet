cfcf2dc817493f05aa0ea562ecb3ae88ed6036b8
Craig Mulligan <git@craigmulligan.com>
2024-05-05 15:36:42 -0400

    clean up auth check

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -10,42 +10,41 @@ from app.db import get_db
 router = APIRouter()
 templates = Jinja2Templates(directory="app/templates")
 
-@router.get("/")
-def dashboard_get(request: Request, db: Session = Depends(get_db)):
+async def current_user_id(request: Request):
     user_id: str | None = request.cookies.get("user_id")
+    return user_id 
 
-    if not user_id:
+
+@router.get("/")
+def dashboard_get(request: Request, db: Session = Depends(get_db), current_user_id: str | None = Depends(current_user_id)):
+    if not current_user_id:
         # Not logged in.
         return RedirectResponse("/signin")
 
-    user = db.query(models.User).filter(models.User.id==user_id).one()
+    user = db.query(models.User).filter(models.User.id==current_user_id).one()
 
     print(user.leaflets)
 
     return templates.TemplateResponse(request, "dashboard.html", {"user": user })
 
 @router.get("/leaflet/{leaflet_id}")
-def dashboard_leaflet_get(leaflet_id: int, request: Request, db: Session = Depends(get_db)):
-    user_id: str | None = request.cookies.get("user_id")
-
-    if not user_id:
+def dashboard_leaflet_get(leaflet_id: int, request: Request, db: Session = Depends(get_db), current_user_id: str | None = Depends(current_user_id)):
+    if not current_user_id:
         # Not logged in.
         return RedirectResponse("/signin")
 
-    user = db.query(models.User).filter(models.User.id==user_id).one()
+    user = db.query(models.User).filter(models.User.id==current_user_id).one()
     leaflet = db.query(models.Leaflet).filter_by(id=leaflet_id).one()
 
     return templates.TemplateResponse(request, "leaflet.html", {"user": user, "leaflet": leaflet })
 
 @router.get("/recipe/{recipe_id}")
-def dashboard_recipe_get(recipe_id: int, request: Request, db: Session = Depends(get_db)):
-    user_id: str | None = request.cookies.get("user_id")
-
-    if not user_id:
+def dashboard_recipe_get(recipe_id: int, request: Request, db: Session = Depends(get_db), current_user_id: str | None = Depends(current_user_id)):
+    if not current_user_id:
         # Not logged in.
         return RedirectResponse("/signin")
 
-    user = db.query(models.User).filter(models.User.id==user_id).one()
+    user = db.query(models.User).filter(models.User.id==current_user_id).one()
     recipe = db.query(models.Recipe).filter_by(id=recipe_id).one()
 
     return templates.TemplateResponse(request, "recipe.html", {"user": user, "recipe": recipe })
